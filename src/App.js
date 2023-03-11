745d6bc5e635cc084871740dd8755f8dc40e9904
LeonCookie <LeonGdynia1105@gmail.com>
2023-03-11 22:29:07 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css';  
 
+function goToAuthor() {
+  window.open("../Author.html");
+}
 function App() {
+
+
   return (
-    <div>
-      <h1>SudoGame</h1>
+    
+    
+    <div id="MainDiv">
+      <div id="Sokoban_Title">
     </div>
+    <div id="MenuDiv">
+    <h1 class>Sokoban</h1>      
+      <button id="StartBtn"class="button-54" >Start</button>
+      <br></br>
+      <button id="AuthorBtn" class="button-54" onClick={goToAuthor  }>Author</button>
+    </div>
+    </div>
+
+   
+    
+    
   );
 }
 
